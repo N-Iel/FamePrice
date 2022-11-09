7b0561a9ddb4730c5a417a1d87f9335f41be5707
Daniel Palacios <palaciosp.daniel@outlook.com>
2022-11-09 16:59:43 +0100

    Added Fan and corresponding IA

---
+++ Assets/Scripts/EnemyController.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemyController : MonoBehaviour
+{
+
+    public Transform player;
+
+    NavMeshAgent agent;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Follow();
+    }
+
+    void Follow()
+    {
+        agent.SetDestination(player.transform.position);
+    }
+}
